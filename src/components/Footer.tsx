9eb44c12f94f74e1fe309875b2abda2468694684
nawawi489 <aksalalshal18@gmail.com>
2026-01-12 00:30:20 +0800

    chore: update footer year to be dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer" aria-label="Footer">
       <div className="container" style={{ textAlign: 'center' }}>
-        © 2025 Nyantuy Group
+        © {new Date().getFullYear()} Nyantuy Group
       </div>
     </footer>
   )
